7ebd62024429e35f8c76e01bd6dc3c5856b2c5da
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2018-06-07 12:41:26 -0700

    Fix intro text in registration for awesome support

--- awesome-support/registration.php
+++ awesome-support/registration.php
@@ -48,9 +48,9 @@ $wrapper_class = 'allow' !== $registration ? 'wpas-login-only' : 'wpas-login-reg
 		</div>
 		<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 		  <div class="panel-body">
-		  	<?php do_action('wpas_before_login_form'); ?>
-		  	
-		  	<p class="lead">If you are not currently a student, you can create an account in order to submit requests</p>
+			<p class="lead">If you are not currently a student, you can create an account in order to submit requests</p>
+			<?php do_action('wpas_before_login_form'); ?>
+
 	
 			<form class="wpas-form" id="wpas_form_login" method="post" role="form" action="<?php echo wpas_get_login_url(); ?>">
 				<h3><?php _e( 'Log in', 'awesome-support' ); ?></h3>
