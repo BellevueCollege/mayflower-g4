588d973280652d284991122025f5e16c4bebfaec
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2018-10-25 13:08:37 -0700

    Update Course Description Shortcode to use Data API
    Resolves MAYF-61. Course shortcode itself now pulls
    from Data API instead of Class Schedule API. The shortcode
    builder still uses the CS API.

--- inc/mayflower-course-descriptions/mayflower-course-descriptions.php
+++ inc/mayflower-course-descriptions/mayflower-course-descriptions.php
@@ -157,98 +157,70 @@ function get_course_callback() {
 }
 
 //function for rendering single course shortcode into HTML
-function coursedescription_func($atts)
-{
-      $subject = $atts["subject"];
-      $course = $atts["courseid"];// Attribute name should always read in lower case.
-    $description = $atts["description"];
-    if(!empty($course) && !empty($subject))
-    {
-        //error_log("course :".$course);
-        $course_split = explode(" ",$course);
-        $course_letter = $course_split[0];
-        $course_id = $course_split[1];
-        $subject = trim(html_entity_decode  ($subject));
-        $url = "http://www.bellevuecollege.edu/classes/All/".$subject."?format=json";
-        $json = wp_remote_get($url);
-        if(!empty($json) && !empty($json['body']))
-        {
-		    $html = decodejsonClassInfo($json['body'],$course_id,$description);
-            return $html;
-        }
-    }
-    return null;
+function coursedescription_func( $atts ) {
+	$subject = $atts["subject"];
+	$course = $atts["courseid"];// Attribute name should always read in lower case.
+	$description = $atts["description"];
+	if ( ! empty( $course ) && ! empty( $subject ) ) {
+		$course_split = explode( " ", $course );
+		$course_letter = $course_split[0];
+		$course_id = $course_split[1];
+		$subject = trim( html_entity_decode( $subject ) );
+		// $url = "http://www.bellevuecollege.edu/classes/All/".$subject."?format=json";
+
+		$url = 'https://www.bellevuecollege.edu/data/api/v1/course/' . urlencode( $subject ) . '/'. urlencode( trim( $course_id ) );
+		$data = wp_remote_get( $url );
+
+		if ( ! empty( $data ) && ! empty ( $data['body'] ) && ! empty ( $data['body'] ) ) {
+			$json = json_decode( $data['body'] );
+			$html = mayflower_decode_json_class_info( $json, $description );
+			return $html;
+		}
+	}
+	return null;
 }
 
 //process json returned by API call
-function decodejsonClassInfo($jsonString,$number = NULL,$description = NULL)
-{
-    $decodeJson = json_decode($jsonString,true);
-    $htmlString = "";
-    $courses = $decodeJson["Courses"];
-    $htmlString .= "<div class='classDescriptions'>";
-    if(count($courses)>0)
-    {
-        foreach($courses as $sections)
-        {
-            if($number!=null)
-            {
-                if($sections["Number"] == $number)
-                {
-                    $htmlString .= getHtmlForCourse($sections,$description);
-                }
-            }
-            else
-            {
-                $htmlString .= getHtmlForCourse($sections,$description);
-            }
-        }
-    }
-    $htmlString .= "</div>"; //classDescriptions
+function mayflower_decode_json_class_info( $json, $show_description = false ) {
+	// return print_r( $course, true );
+	$htmlString = '';
+	$htmlString .= '<div class="classDescriptions">';
 
-    return $htmlString;
+	$htmlString .= mayflower_course_html( $json, $show_description );
+
+	$htmlString .= "</div>"; //classDescriptions
+
+	return $htmlString;
 }
 
-//process individual course for display
-function getHtmlForCourse($sections,$description = NULL)
-{
-    $htmlString = "";
-    $htmlString .= "<div class='class-info'>";
-    $htmlString .= "<h5 class='class-heading'>";
-        $courseUrl = CLASSESURL.$sections["Subject"];
-        if($sections["IsCommonCourse"])
-        {
-            $courseUrl .= "&";
-        }
-        $courseUrl .= "/".$sections["Number"];
 
-        $htmlString .= "<a href='".$courseUrl."''>";
-        $htmlString .= "<span class='course-id'>".$sections["Descriptions"][0]["CourseID"]."</span>";
-        $htmlString .= " <span class='course-title'>".$sections["Title"]."</span>";
-        $htmlString .= "<span class='course-credits'> &#8226; ";
+function mayflower_course_html( $data, $show_description = false ) {
+	if ( isset( $data->course ) ) { //if there is course data, return course information
+		$course = $data->course;
+		$title = $course->subject . ' '
+		. $course->courseNumber . ': ' 
+		. $course->title . ' - ' 
+		. ( $course->isVariableCredits ? 'variable' : $course->credits ) . ' credits';
 
-        if($sections["IsVariableCredits"])
-        {
-            $htmlString .= "V1-".$sections["Credits"]." <abbr title='variable credit'>Cr.</abbr>";
-        }
-        else
-        {
-            $htmlString .= $sections["Credits"]." <abbr title='credit(s)'>Cr.</abbr>";
-        }
-        $htmlString .= "</span>";
-        $htmlString .= "</a>";
-        $htmlString .= "</h5>";//classHeading
-    if($description=="true" && !empty($sections["Descriptions"]))
-    {
-        //error_log("Not here");
-        $htmlString .= "<p class='class-description'>" . $sections["Descriptions"][0]["Description"] . "</p>";
-        $htmlString .= "<p class='class-details-link'>";
-        $htmlString .= "<a href='".$courseUrl."'>View details for ".$sections["Descriptions"][0]["CourseID"]."</a>";
-        $htmlString .= "</p>";
-    }
-        $htmlString .= "</div>"; //classInfo
-        return $htmlString;
+		$url = 'https://www.bellevuecollege.edu/classes/All/' . $course->subject . '/' .
+			$course->courseNumber;
+
+		$description = $course->description;
+
+		$more = 'View details for ' . $course->subject . ' '
+			. $course->courseNumber;
+
+		if ( $show_description && 'false' != $show_description ) {
+			return "<h3><a href='$url'>$title</a></h3><p>$description</p><p><a href='$url'>$more</a></p>";
+		} else {
+			return "<h3><a href='$url'>$title</a></h3>";
+		}
+
+	} else { //if course not selected, return to inform user there are courses available
+		return '<!-- No Course Available -->';
+	}
 }
+
 
 /*****************************
 * Multiple courses functions
