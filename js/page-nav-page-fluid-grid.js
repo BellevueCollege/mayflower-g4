4af42d1ef0ce2310988dda97c7e856e343cc5659
Angela Vu <angela.vu@bellevuecollege.edu>
2018-11-08 13:21:45 -0800

    Add Check to Init Masonry if Fluid Grid Exists
    Prevents Masonry errors if fluid grid template is selected
    for the Gutenberg editor.
    
    Resolves MB-15.

--- js/page-nav-page-fluid-grid.js
+++ js/page-nav-page-fluid-grid.js
@@ -1,15 +1,18 @@
 // init Masonry
 var grid = document.querySelector('.child-pages');
 
-var msnry = new Masonry(grid, {
-    itemSelector: '.child-pages article',
-    columnWidth: '.child-pages .grid-sizer',
-    percentPosition: true,
-    horizontalOrder: true,
-    gutter:10
-});
+if (grid != null){ //check if grid is not null/undefined then construct Masonry
+    var msnry = new Masonry(grid, {
+        itemSelector: '.child-pages article',
+        columnWidth: '.child-pages .grid-sizer',
+        percentPosition: true,
+        horizontalOrder: true,
+        gutter:10
+    });
+    
+    imagesLoaded(grid).on('progress', function () {
+        // layout Masonry after each image loads
+        msnry.layout();
+    });
 
-imagesLoaded(grid).on('progress', function () {
-    // layout Masonry after each image loads
-    msnry.layout();
-});
+}
