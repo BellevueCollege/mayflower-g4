c0cd83a3fd92e766b75b9328852f075ebf18bf3e
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2019-11-21 14:05:00 -0800

    🐛 FIX: Staff Page Style on G4

--- parts/single-staff.php
+++ parts/single-staff.php
@@ -8,9 +8,8 @@
 if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post(); ?>
-	<div class="media">
-		<div class="staff-details-card">
-			<a class="pull-left" href="<?php the_permalink(); ?>">
+	<div class="media flex-column flex-md-row">
+			<a class="align-self-start mr-3 mb-4" href="<?php the_permalink(); ?>">
 			<?php
 			if ( has_post_thumbnail() ) {
 				the_post_thumbnail(
@@ -32,63 +31,53 @@ if ( have_posts() ) :
 			</a>
 
 			<div class="media-body">
-				<div class="caption staff-details staff-details-top">
-					<?php $post_meta_data = get_post_custom( $post->ID ); ?>
-						<div class="staff-details-header">
-							<h2><?php the_title(); ?></h2>
+				<?php $post_meta_data = get_post_custom( $post->ID ); ?>
+				<h2 class="my-0"><?php the_title(); ?></h2>
 
-							<?php if ( isset( $post_meta_data['_staff_position'][0] ) ) { ?>
-								<h3><?php echo esc_attr( $post_meta_data['_staff_position'][0] ); ?></h3>
-							<?php } ?>
-						</div>
+				<?php if ( isset( $post_meta_data['_staff_position'][0] ) ) { ?>
+					<h3 class="mt-0"><?php echo esc_attr( $post_meta_data['_staff_position'][0] ); ?></h3>
+				<?php } ?>
 
-						<ul>
-							<?php if ( isset( $post_meta_data['_staff_email'][0] ) ) { ?>
-								<li>
-									<strong>Email: </strong>
-									<a href="mailto:<?php echo esc_attr( $post_meta_data['_staff_email'][0] ); ?>"><?php echo esc_attr( $post_meta_data['_staff_email'][0] ); ?></a>
-								</li>
-							<?php } ?>
+				<ul>
+					<?php if ( isset( $post_meta_data['_staff_email'][0] ) ) { ?>
+						<li>
+							<strong>Email: </strong>
+							<a href="mailto:<?php echo esc_attr( $post_meta_data['_staff_email'][0] ); ?>"><?php echo esc_attr( $post_meta_data['_staff_email'][0] ); ?></a>
+						</li>
+					<?php } ?>
 
-							<?php if ( isset( $post_meta_data['_staff_phone'][0] ) ) { ?>
-								<li>
-									<strong>Phone: </strong>
-									<?php echo esc_attr( $post_meta_data['_staff_phone'][0] ); ?>
-								</li>
-							<?php } ?>
+					<?php if ( isset( $post_meta_data['_staff_phone'][0] ) ) { ?>
+						<li>
+							<strong>Phone: </strong>
+							<?php echo esc_attr( $post_meta_data['_staff_phone'][0] ); ?>
+						</li>
+					<?php } ?>
 
-							<?php if ( isset( $post_meta_data['_staff_office_location'][0] ) ) { ?>
-								<li>
-									<strong>Office Location: </strong>
-									<?php echo esc_attr( $post_meta_data['_staff_office_location'][0] ); ?>
-								</li>
-							<?php } ?>
+					<?php if ( isset( $post_meta_data['_staff_office_location'][0] ) ) { ?>
+						<li>
+							<strong>Office Location: </strong>
+							<?php echo esc_attr( $post_meta_data['_staff_office_location'][0] ); ?>
+						</li>
+					<?php } ?>
 
-							<?php if ( isset( $post_meta_data['_staff_office_hours'][0] ) ) { ?>
-								<li>
-									<strong>Office Hours: </strong>
-									<?php echo esc_attr( $post_meta_data['_staff_office_hours'][0] ); ?>
-								</li>
-							<?php } ?>
+					<?php if ( isset( $post_meta_data['_staff_office_hours'][0] ) ) { ?>
+						<li>
+							<strong>Office Hours: </strong>
+							<?php echo esc_attr( $post_meta_data['_staff_office_hours'][0] ); ?>
+						</li>
+					<?php } ?>
 
-						</ul>
+				</ul>
 
-				</div><!-- caption -->
 			</div><!-- media-body -->
-			<div class="row">
-				<div class="col-12 staff-details">
-					<div class="staff-biography-single">
-						<?php if ( ! empty( $post->post_content ) ) { ?>
-							<h3 class="staff-biography">Biography:</h3>
-							<?php the_content(); ?>
-						<?php } ?>
-					</div>
-				</div> <!-- col-sm-12 -->
-			</div> <!-- row -->
-		</div> <!-- staff-details-card -->
-	</div><!-- media -->
 
-		<?php endwhile; ?>
+		</div><!-- media -->
+		<?php if ( ! empty( $post->post_content ) ) { ?>
+			<h3>Biography:</h3>
+			<?php the_content(); ?>
+		<?php } ?>
+
+	<?php endwhile; ?>
 
 	<?php wp_reset_postdata();
 endif; ?>
