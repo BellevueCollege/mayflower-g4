5b9a69c36c27fc3d7fd3836dc351747bc1d1e75b
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2020-08-10 11:53:22 -0700

    🐛 FIX: Disable WP Buttons Block
    Disable core/buttons, as core/button is already disabled. MAYF-115

--- js/blocks-blacklist.js
+++ js/blocks-blacklist.js
@@ -11,6 +11,7 @@ window.onload = function () {
 	wp.blocks.unregisterBlockType('core/columns');
 	wp.blocks.unregisterBlockType('core/column');
 	wp.blocks.unregisterBlockType('core/button');
+	wp.blocks.unregisterBlockType('core/buttons');
 	wp.blocks.unregisterBlockType('core/media-text');
 	wp.blocks.unregisterBlockType('core/more');
 	wp.blocks.unregisterBlockType('core/search');
